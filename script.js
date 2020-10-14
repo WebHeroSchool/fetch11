f55804b4db17a1b59a3fd7de6eb2b205c3560fa7
DESKTOP-UC324SV\tonka <tonkastar@mail.ru>
2020-10-14 04:49:55 +0300

    no message

--- script.js
+++ script.js
@@ -23,36 +23,36 @@ const getDate = new Promise ((resolve, reject) => {
 
 
 Promise.all([getName, getDate])
+  .then (([request, now]) => fetch(`https://api.github.com/users/${request(href)}`)
   .then ((now) => {
     const time = document.createElement('p');
     time.innerHTML = now;
   })
-  .then ((request) => fetch(`https://api.github.com/users/${request(href)}`)
   .then(rep => rep.json())
   .then(json => {
-  	const name = document.createElement('a');
-  	if (json.name != null) {
+    const name = document.createElement('a');
+    if (json.name != null) {
       name.innerHTML = json.name;
       name.href = json.html_url;
-  	} else {
-  		name.innerHTML = 'Информация о пользователе не доступна'
-  	}
-  	  body.append(name);
+    } else {
+      name.innerHTML = 'Информация о пользователе не доступна'
+    }
+      body.append(name);
 
-  	const img = document.createElement('img');
-  	if (json.avatar_url != null) {
+    const img = document.createElement('img');
+    if (json.avatar_url != null) {
       img.src = json.avatar_url;
-  	} else {
-  		img.src = 'Информация о пользователе не доступна'
-  	}
-  	  body.append(img);
+    } else {
+      img.src = 'Информация о пользователе не доступна'
+    }
+      body.append(img);
 
-  	const bio = document.createElement('p');
-  	if (json.bio != null) {
+    const bio = document.createElement('p');
+    if (json.bio != null) {
       bio.innerHTML = json.bio;
-  	} else {
-  		bio.innerHTML = 'Информация о пользователе не доступна'
-  	}
-  	  body.append(bio);
+    } else {
+      bio.innerHTML = 'Информация о пользователе не доступна'
+    }
+      body.append(bio);
   })
-  .catch(err => console.log(err));
+  .catch(err => console.log(err)))
